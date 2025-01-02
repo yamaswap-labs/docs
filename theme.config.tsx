14bfbc01af1b012f05face105191df4f327a8638
Yamaswap Offical <team@yamaswap.com>
2025-01-02 14:34:32 +0800

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template'
+  },
+  chat: {
+    link: 'https://x.com/0xkkdemian'
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    content: 'kk Nextra Docs Template'
+  }
+};
+
+export default config;
